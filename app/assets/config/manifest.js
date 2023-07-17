4e05cd61180744a91237114b5ed59847f282b158
AlexDelasalle <alex_delasalle@aol.co.uk>
2023-07-17 12:23:27 +0100

    Add image files to asset manifest

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -2,3 +2,5 @@
 //= link_directory ../stylesheets .css
 //= link_tree ../../javascript .js
 //= link_tree ../../../vendor/javascript .js
+//= link-tree ..images/Subject5.png
+//= link-tree ..images/logo.jpeg
